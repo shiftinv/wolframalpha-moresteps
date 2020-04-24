674545dcaf29668cd0ea268e741bbf9cd1da022e
Simon <me@shiftinv.cc>
2020-04-24 21:45:29 +0200

    Keep static files in separate directory, add clean npm-script

--- scripts/build_target.js
+++ scripts/build_target.js
@@ -1,15 +1,16 @@
-const fs = require('fs');
+/**
+ * Script for copying static resources/files and building a browser-specific extension manifest
+ *
+ * Usage: node scripts/build_target.js chrome|firefox
+ */
+
+const fs = require('fs-extra');
 const path = require('path');
 const merge = require('deepmerge');
 
 
-const targets = ['chrome', 'firefox'];
-
 const sourceDir = 'src/';
 const targetDir = 'dist/';
-
-
-const webextPolyfill = 'js/lib/webextension-polyfill@0.6.0.js'
 
 const manifestExtras = {
     chrome: {},
@@ -23,42 +24,70 @@ const manifestExtras = {
 };
 
 
+const _err = console.error.bind(console, '[-]');
+const _log = console.log.bind(console, '[+]');
+const _info = console.info.bind(console, '[i]');
+
 function usage() {
-    console.error('Invalid arguments!');
-    console.error(
-        `>> Usage: ${process.argv[0]} ${process.argv[1]} (${targets.join('|')})`
-    );
+    _err('Invalid arguments!');
+    _err(`>> Usage: ${process.argv[0]} ${process.argv[1]} (${targets.join('|')})`);
     process.exit(1);
 }
 
+
+function checkDirs() {
+    _info('Checking directories');
+
+    if (!fs.existsSync(sourceDir)) {
+        _err(`Source directory '${sourceDir}' does not exist!`);
+        process.exit(1);
+    }
+
+    if (!fs.existsSync(targetDir)) {
+        _info(`Target directory '${targetDir}' does not exist, creating...`);
+        fs.mkdirSync(targetDir);
+    }
+}
+
 function patchManifest(obj) {
+    _info(`Merging manifest.json with:\n${JSON.stringify(obj, null, 4)}`);
+
     // read base manifest
     const manifest = JSON.parse(fs.readFileSync(path.join(sourceDir, 'manifest.json'), 'utf8'));
 
     // merge with extra values
-    console.log(`Merging manifest.json with:\n${JSON.stringify(obj, null, 4)}`);
     const newManifest = merge(manifest, obj);
 
     // write new manifest
     fs.writeFileSync(path.join(targetDir, 'manifest.json'), JSON.stringify(newManifest, null, 4), 'utf8');
 }
 
+function copyStaticFiles() {
+    _info('Copying static files');
+    fs.copySync(path.join(sourceDir, 'static'), path.join(targetDir));
+}
+
+
 // check args
 if (process.argv.length !== 3) usage();
 
 const target = process.argv[2];
-if (!targets.includes(target)) usage();
+if (!Object.keys(manifestExtras).includes(target)) usage();
 
 
 // handle targets
+checkDirs();
 patchManifest(manifestExtras[target]);
+copyStaticFiles();
 
 switch (target) {
 case 'chrome':
     break;
 case 'firefox':
     // remove webext polyfill, not required for firefox
-    fs.truncateSync(path.join(targetDir, webextPolyfill));
+    fs.truncateSync(path.join(targetDir, 'js/lib/webextension-polyfill@0.6.0.js'));
     break;
 }
 
+
+_log('Done.');
